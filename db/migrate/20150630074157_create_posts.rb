d7401fdef82ba4344a7a5a57c381455732b5fb4b
melgomez <meldarrelgomez@gmail.com>
2015-06-30 15:45:55 +0800

    Migrate initial database.

--- db/migrate/20150630074157_create_posts.rb
+++ db/migrate/20150630074157_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.boolean :published
+      t.boolean :published, default: false
       t.datetime :publish_date
 
       t.timestamps null: false
