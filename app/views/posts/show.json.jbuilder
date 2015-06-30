4fb280436da360e63da27e20ff8f11828629a1fc
melgomez <meldarrelgomez@gmail.com>
2015-06-30 15:44:24 +0800

    Initialise upload cvs app with posts scaffold.

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :body, :published, :publish_date, :created_at, :updated_at
---
+++ db/migrate/20150630074157_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.boolean :published
+      t.datetime :publish_date
+
+      t.timestamps null: false
+    end
+  end
+end
